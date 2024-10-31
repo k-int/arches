99ba1f524c0a388dbc1b054215f7afb2d58c3bab
Christopher Byrd <chrabyrd@gmail.com>
2024-10-31 12:24:09 -0700

    removes declarations && updates vitest config, removes sample index and related tests #11591

--- arches/management/commands/updateproject.py
+++ arches/management/commands/updateproject.py
@@ -1,4 +1,8 @@
+import os
+import shutil
+
 from django.core.management.base import BaseCommand
+from arches.app.models.system_settings import settings
 
 
 class Command(BaseCommand):  # pragma: no cover
@@ -18,4 +22,32 @@ class Command(BaseCommand):  # pragma: no cover
             self.stdout.write("Operation aborted.")
 
     def update_to_v8(self):
-        pass
+        # Replaces vitest config files
+        self.stdout.write("Updating vitest configuration files...")
+
+        for config_file in [
+            "vitest.config.mts",
+            "vitest.setup.mts",
+        ]:
+            self.stdout.write("Copying {} to project directory".format(config_file))
+            shutil.copy2(
+                os.path.join(
+                    settings.ROOT_DIR, "install", "arches-templates", config_file
+                ),
+                os.path.join(settings.APP_ROOT, ".."),
+            )
+
+        self.stdout.write("Done!")
+
+        # Removes unnecessary files
+        self.stdout.write("Removing unnecessary files...")
+
+        declarations_test_file_path = os.path.join(
+            settings.APP_ROOT, "src", settings.APP_NAME, "declarations.test.ts"
+        )
+
+        if os.path.exists(declarations_test_file_path):
+            self.stdout.write("Deleting {}".format("declarations.test.ts"))
+            os.remove(declarations_test_file_path)
+
+        self.stdout.write("Done!")
